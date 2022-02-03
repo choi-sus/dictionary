7b8acdf10ce7a3e6debeab0c7f301a4daea2a777
choi-sus <choi-sus@naver.com>
2022-02-03 13:04:28 +0900

    리액트 프로젝트 나만의 사전

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Link } from 'react-router-dom';
 import './App.css';
+import Home from './Home';
+import Add from './Add';
+import Update from './Update';
+import { useDispatch } from "react-redux";
+import { createWord, loadWordFB } from "./redux/modules/word";
+import {db} from "./firebase"
+import { collection, doc, getDoc, getDocs, addDoc } from "firebase/firestore";
 
 function App() {
+  const dispatch = useDispatch();
+  React.useEffect(()=> {
+    dispatch(loadWordFB());
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header>
+        <Link to="/">
+          <h1>나만의 사전</h1>
+        </Link>
       </header>
+      <Route exact path="/">
+        <Home></Home>
+        <Link to="/add">
+          <div className='addPage'>
+            <span></span>
+            <span></span>
+          </div>
+        </Link>
+      </Route>
+      <Route exact path="/add">
+        <Add></Add>
+      </Route>
+      <Route exact path="/update/:id">
+        <Update></Update>
+      </Route>
     </div>
   );
 }
